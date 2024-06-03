06bb25f1fc3e5ab0774489e65a1f8d0f6846d5f7
Z4R1R <156419550+Z4R1R@users.noreply.github.com>
2024-06-03 21:34:17 +0600

    main.py

--- A Random Guy giving Codes/main.py
+++ A Random Guy giving Codes/main.py
@@ -3,6 +3,9 @@ from discord.ext import commands
 import asyncio
 import os
 
+from keep_alive import keep_alive
+keep_alive()
+
 intents = discord.Intents.default()
 intents.message_content = True
 
